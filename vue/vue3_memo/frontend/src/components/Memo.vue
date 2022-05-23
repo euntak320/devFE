da91e722aab8926ac1c97ade29fd872008c04125
tak320 <tak320@naver.com>
2022-05-23 21:52:52 +0900

    데이터 배열 추가

--- vue/vue3_memo/frontend/src/components/Memo.vue
+++ vue/vue3_memo/frontend/src/components/Memo.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="memo">
     <ul>
-      <li>메모 1 내용</li>
-      <li>메모 2 내용</li>
-      <li>메모 3 내용</li>
-      <li>메모 4 내용</li>
-      <li>메모 5 내용</li>
+      <li v-for="(d, i) in data" :key="i">{{ d }}</li>
     </ul>
   </div>
 </template>
@@ -13,7 +9,28 @@
 <style lang="scss" scoped>
     .memo ul {
         list-style: none;
-        padding:0;
+        padding: 15px 0;
         margin:0;
+        li {
+            padding: 5px 10px;
+            margin:5px;
+        }
     }
 </style>
+
+<script>
+    export default {
+        setup() {
+            const data = [
+                "메모 1 내용",
+                "메모 2 내용",
+                "메모 3 내용",
+                "메모 4 내용",
+                "메모 5 내용",
+            ]
+
+            return{ data };
+        }
+    }
+
+</script>
