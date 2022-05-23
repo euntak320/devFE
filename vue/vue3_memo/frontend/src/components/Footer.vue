4c180ae95728ecbb636c58c63e60a61e4b3f3554
tak320 <tak320@naver.com>
2022-05-23 21:44:20 +0900

    정리 및 scss 추가

--- vue/vue3_memo/frontend/src/components/Footer.vue
+++ vue/vue3_memo/frontend/src/components/Footer.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="header">I am footer</div>
+  <footer>&copy; Memo App. All right reseved.</footer>
 </template>
 
-<script>
-export default {
-  
-}
-</script>
+<style scoped>
+  footer {
+    background: #f7f7f7;
+    border-top:1px solid #eee;
+    padding:25px;
+  }
+</style>
--- vue/vue3_memo/frontend/src/components/Header.vue
+++ vue/vue3_memo/frontend/src/components/Header.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="ground">I am ground</div>
+  <header>Memo App</header>
 </template>
 
-<script>
-export default {
-  
+<style scoped>
+/* scoped 해당 파일에서만 style 적용 */
+header {
+  text-align: center;
+  padding:15px;
+  border-bottom: 1px solid #ccc;
+  background: #f7f7f7;
 }
-</script>
+</style>
