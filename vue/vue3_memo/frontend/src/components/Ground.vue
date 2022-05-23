554d8f96377df08ea78be8879179d49e51351567
tak320 <tak320@naver.com>
2022-05-23 21:29:46 +0900

    기본 레이아웃 정리

--- vue/vue3_memo/frontend/src/components/Ground.vue
+++ vue/vue3_memo/frontend/src/components/Ground.vue
@@ -1,9 +1,20 @@
 <template>
-  <div class="ground">I am ground</div>
+  <div class="ground">
+    <Header/>
+    <div class="">Content</div>
+    <Footer/>
+  </div>
+  
 </template>
 
 <script>
+import Header from "./Header.vue"
+import Footer from "./Footer.vue"
 export default {
+  components :{
+    Header,
+    Footer
+  }
   
 }
 </script>
