5af1010b5fa16ec74e8a0886755b0b60b261f0bd
tak320 <tak320@naver.com>
2022-05-25 21:18:04 +0900

    axios 설치

--- vue/vue3_memo/backend/index.js
+++ vue/vue3_memo/backend/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const memos = ["메모1", "메모2", "메모3"];
+
+app.get('/memos', (req, res) => {
+  res.send(memos)
 })
 
 app.listen(port, () => {
